d5ad651981ba7e71b73ba09d02966b7df952b159
Boaz Arsav <9004955@student.tcrmbo.nl>
2022-06-07 13:48:55 +0200

    Speler Gemaakt

--- app.js
+++ app.js
@@ -4,40 +4,54 @@ const c = canvas.getContext('2d')
 canvas.width = innerWidth
 canvas.height = innerHeight
 
-
+//alles voor de speler
 class Player {
     constructor() {
-        this.position = {
-            x: 200,
-            y: 200
-        }
 
+        
         this.velocity = {
             x: 0,
             y: 0
         }
 
+        //spelermodel inladen en instellingen van spelermodel
         const image = new Image()
         image.src = './img/spaceship.png'
+        image.onload = () => {
+            const scale = 0.15
+            this.image = image
+            this.width = image.width * scale
+            this.heigth = image.height * scale
+            //hiermee bepaal je de positie van de speler op spawnpoint
+            this.position = {
+                x: canvas.width / 2 - this.width / 2,
+                y: canvas.height - this.heigth - 20
+            }
+        }
 
-        this.image = image
-        this.width = 100
-        this.heigth = 100
+
     }
 
     draw() {
         // c.fillStyle = 'red'
         // c.fillRect(this.position.x, this.position.y, this.width, this.height)
-        c.drawImage(this.image, this.position.x, this.position.y)
+        if (this.image)
+        c.drawImage(
+            this.image,
+            this.position.x,
+            this.position.y,
+            this.width,
+            this.heigth)
     }
 }
 
 const player = new Player()
 player.draw()
 
-function animate(){
+function animate() {
     requestAnimationFrame(animate)
-    c.fillRect (0,0, canvas.width, canvas.height)
+    c.fillStyle = 'black'
+    c.fillRect(0, 0, canvas.width, canvas.height)
     player.draw()
 }
 animate()
