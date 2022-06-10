3bf889e0b1ec0f5c2eb6b33d9c2c29ab43d469f9
Boaz Arsav <9004955@student.tcrmbo.nl>
2022-06-10 11:52:42 +0200

    pushypushy voor de forky forky

--- app.js
+++ app.js
@@ -1,84 +1,99 @@
-//kijk video vanaf 22:42
-const canvas = document.querySelector('canvas')
-const c = canvas.getContext('2d')
+//kijk video vanaf 30:00
+const canvas = document.querySelector("canvas");
+const c = canvas.getContext("2d");
 
-canvas.width = innerWidth
-canvas.height = innerHeight
+canvas.width = innerWidth;
+canvas.height = innerHeight;
 
-//Hoofd instellingen voor de speler
 class Player {
-    constructor(){
-        //bewegingsnelheid voor de speler
-        this.velocity = {
-            x: 0,
-            y: 0
-        }
-        //laad de foto in voor de speler
-        const image = new Image()
-        image.src = './img/spaceship.png'
-        image.onload = () => {
-            const scale = 0.15
-            this.image = image
-            this.width = image.width * scale
-            this.height = image.height * scale
+  constructor() {
 
-            this.position = {
-            x: canvas.width / 2 - this.width / 2,
-            y: canvas.height - this.height - 20
-        }
-        }
+    this.velocity = {
+      x: 0,
+      y: 0,
+    };
+    //laad de image van de speler in
+    const image = new Image();
+    image.src = "./img/spaceship.png";
+    image.onload = () => {
+      const scale = 0.15;
+      this.image = image;
+      this.width = image.width * scale;
+      this.height = image.height * scale;
+      this.position = {
+        x: canvas.width / 2 - this.width / 2,
+        y: canvas.height - this.height,
+      };
+    };
+  }
 
+  draw() {
+    c.drawImage(
+      this.image,
+      this.position.x,
+      this.position.y,
+      this.width,
+      this.height
+    );
+  }
 
+  update() {
+    if (this.image) {
+      this.draw();
+      this.position.x += this.velocity.x;
     }
-
-    draw() {
-// c.fillStyle = 'red'
-// c.fillRect(this.position.x, this.position.y, this.width, this.height)
-    if (this.image)
-    c.drawImage (
-    this.image,
-    this.position.x,
-    this.position.y,
-    this.width,
-    this.height
-    )
-    }
-
-
-
-    update(){
-        this.draw()
-        this.position.x += this.velocity.x
-    }
+  }
 }
 
+const player = new Player();
+const keys = {
+  a: {
+    pressed: false,
+  },
+  d: {
+    pressed: false,
+  },
+  space: {
+    pressed: false,
+  },
+};
 
+function animate() {
+  requestAnimationFrame(animate);
+  c.fillStyle = "black";
+  c.fillRect(0, 0, canvas.width, canvas.height);
+  player.update();
 
-const player = new Player()
-player.draw()
-
-function animate(){
-    requestAnimationFrame(animate)
-    c.fillStyle = 'black'
-    c.fillRect(0, 0, canvas.width, canvas.height)
-    player.draw()
+  if (keys.a.pressed) {
+    player.velocity.x = -5;
+  } else {
+    player.velocity.x = 0;
+  }
 }
+animate();
 
-animate()
+addEventListener("keydown", ({ key }) => {
+  switch (key) {
+    case "a":
+      keys.a.pressed = true;
+      break;
+    case "d":
+      keys.d.pressed = true;
+      break;
+    case " ":
+      break;
+  }
+});
 
-//test besturing
-addEventListener('keydown', ({key}) => {
-    switch(key){
-        case 'a':
-        console.log('left')
-        break
-
-        case 'd':
-        console.log('right')
-        break
-
-        case ' ':
-        console.log('shoot')
-        break
-    }
-})
+addEventListener("keyup", ({ key }) => {
+  switch (key) {
+    case "a":
+      keys.a.pressed = false;
+      break;
+    case "d":
+      keys.d.pressed = false;
+      break;
+    case " ":
+      break;
+  }
+});
